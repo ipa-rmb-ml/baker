625ed0702adb80be4273404ccefa55520e519c5c
ipa-rmb-ml <44199646+ipa-rmb-ml@users.noreply.github.com>
2018-11-19 16:42:28 +0100

    Add files via upload
    - new structure
    - segmentation class
    - running node in ipa_door_detection_main.cpp

---
+++ ipa_door_handle_detection/ipa_door_handle_detection/include/ipa_door_handle_template_generation.h
@@ -0,0 +1,8 @@
+// based on PointCloudDataImport class 
+// generating the template databasa for various door handle types 
+class PointCloudTemplateDatabase
+{
+public:
+private:
+	PointCloudTemplateDatabase();
+};
