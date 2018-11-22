0e86dd01bebd7fc3af63b80bf0c8b9fce2996ff5
ipa-rmb-ml <44199646+ipa-rmb-ml@users.noreply.github.com>
2018-11-22 16:46:28 +0100

    Add files via upload

--- ipa_door_handle_detection/ipa_door_handle_detection/src/ipa_door_handle_segmentation.cpp
+++ ipa_door_handle_detection/ipa_door_handle_detection/src/ipa_door_handle_segmentation.cpp
@@ -19,7 +19,6 @@ PointCloudSegmentation::PointCloudSegmentation(ros::NodeHandle nh, sensor_msgs::
 
 	if (!ros::ok()){
 		std::cout << "Quit publishing" << std::endl;
-
 	}
 
 	std::cout << "PointCloudSegmentation Constructor Initialised." << std::endl;
@@ -31,26 +30,36 @@ void PointCloudSegmentation::pointcloudCallback(const sensor_msgs::PointCloud2::
 	//create new point cloud in pcl format: pointcloud_in_pcl_format
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_in_pcl_format(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr published_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
+	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr,Eigen::aligned_allocator<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> >clusters_vec_pc;
+
 
 	//transform imported pointcloud point_cloud_msg to pointcloud_in_pcl_format
 	pcl::fromROSMsg(*point_cloud_msg, *pointcloud_in_pcl_format);
 
 	// segment point cloud and detect planes
-	published_pc = segmentPointCloud(pointcloud_in_pcl_format);
+	clusters_vec_pc = segmentPointCloud(pointcloud_in_pcl_format);
 
 
 	// publish changed point cloud
-	pcl::toROSMsg(*published_pc, *point_cloud_out_msg_);
-	point_cloud_out_msg_->header.frame_id = "camera_link";
-	pub_.publish(point_cloud_out_msg_);
+	if (!clusters_vec_pc.empty())
+	{	
+		// concentrate all cluster for visualization
+		for (int numCluster = 0; numCluster < clusters_vec_pc.size (); ++numCluster)
+		{			
+			*published_pc+= *clusters_vec_pc[numCluster];
+		}
 
+		pcl::toROSMsg(*published_pc, *point_cloud_out_msg_);
+		point_cloud_out_msg_->header.frame_id = "camera_link";
+		pub_.publish(point_cloud_out_msg_);
+	}
 }
 
 
 
 // main segmentation process including coloring the pointcloud and the plane detection
 // ================================================================================================================================
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::segmentPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud)
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr,Eigen::aligned_allocator<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> >  PointCloudSegmentation::segmentPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud)
 {
 	
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr plane_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
@@ -60,6 +69,8 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::segmentPointCloud
 
 
 	pcl::ModelCoefficients::Ptr plane_coeff;
+	std::vector <pcl::PointIndices> clusters;
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr,Eigen::aligned_allocator<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> >clusters_vec_pc;
 
 	// PLANE DETECTION 
 	planeInformation planeData = detectPlaneInPointCloud(input_cloud);
@@ -70,12 +81,15 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::segmentPointCloud
 	// coloredPC: PC colored in blue
 	// plane_pc: detected plane ccolored in red
 	// plane coeff: coeffs of the detected plane
+
 	reduced_pc=minimizePointCloudToObject(input_cloud,plane_pc,plane_coeff);
-	clustered_pc=findClustersByRegionGrowing(reduced_pc,plane_pc);
+
+	clusters=findClustersByRegionGrowing(reduced_pc);
+    clusters_vec_pc = generateAlignmentObject(clusters,reduced_pc,plane_coeff);
+
 
 	// concentrate plane_pc with cluster_pc for visualizatuion
-
- return clustered_pc;
+ return clusters_vec_pc;
 }	
 //=====================================================================================================================================
 
@@ -106,6 +120,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::changePointCloudC
 	//std::cout << "<<< Ending PC color change." << std::endl;
 	return pointcloud_xyzrgb;
 }
+
 
 
  	planeInformation PointCloudSegmentation::detectPlaneInPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud){
@@ -148,10 +163,13 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::changePointCloudC
 	planeData.plane_point_cloud = plane_pc;
  	planeData.plane_coeff = plane_coeff;
 
-
 	return planeData;
+};
 
-}
+
+
+
+
 
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::minimizePointCloudToObject(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr plane_pc,pcl::ModelCoefficients::Ptr plane_coeff){
@@ -185,17 +203,22 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::minimizePointClou
 	return reduced_pc;
 }
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::findClustersByRegionGrowing(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud,pcl::PointCloud<pcl::PointXYZRGB>::Ptr plane_pc)
+
+
+
+
+
+
+
+std::vector <pcl::PointIndices> PointCloudSegmentation::findClustersByRegionGrowing(pcl::PointCloud<pcl::PointXYZRGB>::Ptr reduced_pc)
 {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr clustered_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::search::Search<pcl::PointXYZRGB>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZRGB> > (new pcl::search::KdTree<pcl::PointXYZRGB>);
-
-
+  
   // computing normals
   pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
   pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;	
   normal_estimator.setSearchMethod (tree);
-  normal_estimator.setInputCloud (input_cloud);
+  normal_estimator.setInputCloud (reduced_pc);
   normal_estimator.setKSearch (50);
   normal_estimator.compute (*normals);
 
@@ -205,7 +228,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::findClustersByReg
   reg.setMaxClusterSize (1000000);
   reg.setSearchMethod (tree);
   reg.setNumberOfNeighbours (30);
-  reg.setInputCloud (input_cloud);
+  reg.setInputCloud (reduced_pc);
   //reg.setIndices (indices);
   reg.setInputNormals (normals);
 
@@ -214,42 +237,96 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::findClustersByReg
   reg.setSmoothnessThreshold (3.0 / 180.0 * M_PI);
   // curvature threshold if two points have a small normals deviation then the disparity btw their curvatures is tested
   reg.setCurvatureThreshold (1.0);
-
   std::vector <pcl::PointIndices> clusters;
   reg.extract (clusters);
 
-
  // put into new function --> visualizing/counting clusters 
-  std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
 
-pcl::PointXYZRGB clusteredPP;
-// iterate over cluster
-  for (int numCluster =0; numCluster < clusters.size(); numCluster=numCluster +1)
-  {
-
-	// randomize cluster color in pc for visualization
-    uint8_t r,g,b;
-	r = rand() % 255;
-	g = rand() % 255;
-	b = rand() % 255;
-
-	// iterate over points in cluster to color them in diferent colors
-	for (size_t i = 0; i < clusters[numCluster].indices.size (); ++i)
-	{
-		clusteredPP.x=input_cloud->points[clusters[numCluster].indices[i]].x;
-		clusteredPP.y=input_cloud->points[clusters[numCluster].indices[i]].y;
-		clusteredPP.z=input_cloud->points[clusters[numCluster].indices[i]].z; 
-		clusteredPP.r = r;
-		clusteredPP.g = g;
-		clusteredPP.b = b;
-
-		// adding to plane_pc for visualization
-		plane_pc->points.push_back(clusteredPP);
-		// only storing segment object outside the detected plane
-		clustered_pc->points.push_back(clusteredPP);
-
-	} // end points 
-  } // end clusters
-
-	return plane_pc;
+  return clusters;
 }
+
+
+
+
+
+
+	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr,
+	Eigen::aligned_allocator<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> > PointCloudSegmentation::generateAlignmentObject(std::vector <pcl::PointIndices> clusters,pcl::PointCloud<pcl::PointXYZRGB>::Ptr reduced_pc, pcl::ModelCoefficients::Ptr plane_coeff)
+{
+
+    pcl::PointXYZRGB clusteredPP;
+   //write each cluster to as point cloud
+   //vector to store clusters
+	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr,
+	Eigen::aligned_allocator<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> >clusterVec_pc;
+
+	// avoid crashing
+	if(clusters.size() > 0)
+	{
+	// iterate over cluster
+		for (int numCluster =0; numCluster < clusters.size(); numCluster=numCluster +1)
+		{
+			// randomize cluster color in pc for visualization
+			uint8_t r,g,b;
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+				// iterate over points in cluster to color them in diferent colors
+				for (size_t i = 0; i < clusters[numCluster].indices.size (); ++i)
+				{
+					pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster_pt_proj_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
+					pcl::PointXYZRGB clusteredPP_proj;
+
+					clusteredPP.x=reduced_pc->points[clusters[numCluster].indices[i]].x;
+					clusteredPP.y=reduced_pc->points[clusters[numCluster].indices[i]].y;
+					clusteredPP.z=reduced_pc->points[clusters[numCluster].indices[i]].z; 
+					clusteredPP.r =0;
+					clusteredPP.g =0;
+					clusteredPP.b =255;
+
+					// adding single points to point cloud cluster, these are the object point lying outsidee the plane 
+					cluster_pc->points.push_back(clusteredPP);
+
+					// removing point outside the cluster projection on the plane
+					cluster_pt_proj_pc=removePlaneOutlierByClusterOnPlaneProjection(clusteredPP,plane_coeff);
+
+					clusteredPP_proj.x = cluster_pt_proj_pc->points[0].x;
+					clusteredPP_proj.y = cluster_pt_proj_pc->points[0].y;	
+					clusteredPP_proj.z = cluster_pt_proj_pc->points[0].z;
+					clusteredPP_proj.r = 255;
+					clusteredPP_proj.g = 0;
+					clusteredPP_proj.b = 0;
+					cluster_pc->points.push_back(clusteredPP_proj);
+				} 
+
+				clusterVec_pc.push_back(cluster_pc);
+
+		} // end clusters
+		std::cout << "Number of clusters: " << clusters.size () << std::endl;
+		return clusterVec_pc;
+
+	}; //end if
+    
+	std::cout << "No cluster found"<< std::endl;
+	return clusterVec_pc; 
+};
+
+
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudSegmentation::removePlaneOutlierByClusterOnPlaneProjection(pcl::PointXYZRGB clusterPoint,pcl::ModelCoefficients::Ptr plane_coeff)
+{
+   pcl::PointXYZRGB clusterPoint_proj;
+   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cache_pc(new pcl::PointCloud<pcl::PointXYZRGB>);
+   pcl::PointCloud<pcl::PointXYZRGB>::Ptr clusterProj(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+   cache_pc->points.push_back(clusterPoint);
+
+	// plane coefficients: plane_coeff
+	// creating ProjectInliners object --> using plane_coeffs to describe projection plane geometrically
+	pcl::ProjectInliers<pcl::PointXYZRGB> proj;
+	proj.setModelType (pcl::SACMODEL_PLANE);
+	proj.setInputCloud (cache_pc);
+	proj.setModelCoefficients (plane_coeff);
+	proj.filter (*clusterProj);
+
+	return clusterProj;
+
+};
